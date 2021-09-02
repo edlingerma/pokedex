1d0f1714d42d3499b4713bf2ae1a5393e3f14b60
edlingerma <maria.edlinger@gmail.com>
2021-09-02 15:02:21 +0200

    Added tests

---
+++ cypress/integration/main_page.spec.ts
@@ -0,0 +1,21 @@
+describe('Test Mainpage with List and Pagination', () => {
+
+  it('Visit website and check for heading', () => {
+    cy.visit('http://localhost:3000/')
+    cy.contains('Pokédex')
+  })
+
+  it('Check number of pokemons on first page', () => {
+    cy.get('.MuiCard-root', { timeout: 10000 }).should('have.length', 20)
+  })
+
+  it('Change to next page and check if it worked', () => {
+    cy.get('.MuiPagination-root', { timeout: 10000 }).get('li').last().click()
+    cy.contains('pikachu', { timeout: 10000 })
+  })
+
+  it('Click on card of pikachu and change to its detailpage', () => {
+    cy.contains('pikachu', { timeout: 10000 }).click()
+    cy.location('pathname').should('eq', '/:pikachu')
+  })
+})
--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,26 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { Provider } from 'react-redux'
+import {store} from './store/index'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+window.scrollTo = jest.fn();    // fixes Error: Not implemented: window.scrollTo
+
+test('renders title', () => {
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  )
+  const title = screen.getByText('Pokédex');
+  expect(title).toBeInTheDocument();
+})
+
+test('renders name in footer', () => {
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  )
+  const name = screen.getByText('Maria Edlinger');
+  expect(name).toBeInTheDocument();
+})
